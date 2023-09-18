2e1d6f70bd35571e3f6bae1815d85c8439d87800
Mthdk <matheus.karo@aluno.faculdadeimpacta.com.br>
2023-09-18 19:21:46 -0300

    Create main3.py

---
+++ main3.py
@@ -0,0 +1,38 @@
+import os
+import csv
+
+# Diretório que deseja percorrer
+diretorio_base = '/caminho/do/diretorio/base'
+
+# Lista de pastas a serem excluídas da busca
+pastas_a_excluir = ['pasta1', 'pasta2']
+
+# Função para verificar se um arquivo é uma imagem (você pode adicionar mais extensões, se necessário)
+def is_imagem(arquivo):
+    extensoes_imagens = ['.jpg', '.jpeg', '.png', '.gif']
+    return any(arquivo.lower().endswith(ext) for ext in extensoes_imagens)
+
+# Função para contar imagens em uma pasta
+def contar_imagens_em_pasta(pasta):
+    contador = 0
+    for item in os.listdir(pasta):
+        item_caminho = os.path.join(pasta, item)
+        if os.path.isfile(item_caminho) and is_imagem(item):
+            contador += 1
+    return contador
+
+# Abrir um arquivo CSV para escrever as informações
+with open('relatorio.csv', 'w', newline='') as arquivo_csv:
+    escritor_csv = csv.writer(arquivo_csv)
+    escritor_csv.writerow(['Pasta', 'Quantidade de Imagens'])
+
+    # Percorrer o diretório base
+    for pasta_raiz, subpastas, arquivos in os.walk(diretorio_base):
+        for pasta in subpastas[:]:
+            if pasta in pastas_a_excluir:
+                subpastas.remove(pasta)
+        if any(is_imagem(arquivo) for arquivo in arquivos):
+            quantidade_imagens = contar_imagens_em_pasta(pasta_raiz)
+            escritor_csv.writerow([pasta_raiz, quantidade_imagens])
+
+print("Relatório gerado com sucesso em 'relatorio.csv'")
