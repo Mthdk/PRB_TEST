4998af5805ebaa9773c913da390e3177f80258f3
Mthdk <matheus.karo@aluno.faculdadeimpacta.com.br>
2023-09-18 19:35:43 -0300

    Update main3.py

--- main3.py
+++ main3.py
@@ -12,19 +12,21 @@ def is_imagem(arquivo):
     extensoes_imagens = ['.jpg', '.jpeg', '.png', '.gif']
     return any(arquivo.lower().endswith(ext) for ext in extensoes_imagens)
 
-# Função para contar imagens em uma pasta
+# Função para contar imagens em uma pasta e listar os nomes dos arquivos
 def contar_imagens_em_pasta(pasta):
     contador = 0
+    nomes_arquivos = []
     for item in os.listdir(pasta):
         item_caminho = os.path.join(pasta, item)
         if os.path.isfile(item_caminho) and is_imagem(item):
             contador += 1
-    return contador
+            nomes_arquivos.append(item)
+    return contador, nomes_arquivos
 
 # Abrir um arquivo CSV para escrever as informações
 with open('relatorio.csv', 'w', newline='') as arquivo_csv:
     escritor_csv = csv.writer(arquivo_csv)
-    escritor_csv.writerow(['Pasta', 'Quantidade de Imagens'])
+    escritor_csv.writerow(['Pasta', 'Quantidade de Imagens', 'Nomes dos Arquivos'])
 
     # Percorrer o diretório base
     for pasta_raiz, subpastas, arquivos in os.walk(diretorio_base):
@@ -32,7 +34,7 @@ with open('relatorio.csv', 'w', newline='') as arquivo_csv:
             if pasta in pastas_a_excluir:
                 subpastas.remove(pasta)
         if any(is_imagem(arquivo) for arquivo in arquivos):
-            quantidade_imagens = contar_imagens_em_pasta(pasta_raiz)
-            escritor_csv.writerow([pasta_raiz, quantidade_imagens])
+            quantidade_imagens, nomes_arquivos = contar_imagens_em_pasta(pasta_raiz)
+            escritor_csv.writerow([pasta_raiz, quantidade_imagens, ', '.join(nomes_arquivos)])
 
 print("Relatório gerado com sucesso em 'relatorio.csv'")
